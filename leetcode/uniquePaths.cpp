ca6bc9f21fa362934db5baaf7b27450580a09901
ce39906 <ce39906@163.com>
2018-08-29 12:44:46 +0800

    Provide a better solution for uniquePaths

--- leetcode/uniquePaths.cpp
+++ leetcode/uniquePaths.cpp
@@ -26,3 +26,18 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+// better one
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 1);
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp.back();
+    }
+};
